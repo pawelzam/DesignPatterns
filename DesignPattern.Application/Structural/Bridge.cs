b68366f576ba02e7207709371c0200208f995b49
Paweł Zamrij <_PZamrij.AZ@sodexo.com>
2024-04-20 18:07:39 +0200

    More patterns

---
+++ DesignPattern.Application/Structural/Bridge.cs
@@ -0,0 +1,94 @@
+﻿namespace DesignPattern.Application.Structural.Bridge;
+
+// Decouples an abstraction from its implementation so that the two can vary independently. 
+
+#region Usage
+
+static class Program
+{
+    static void Main()
+    {
+        var accountService = new AccountService(new DanskeBankAccountCreator());
+        var danskeBankAccount = accountService.CreateAccount();
+
+        Console.WriteLine(danskeBankAccount.GetType().Name);
+
+        accountService = new AccountService(new CreditBankAccountCreator());
+        var creditBankAccount = accountService.CreateAccount();
+
+        Console.WriteLine(creditBankAccount.GetType().Name);
+    }
+}
+
+#endregion
+
+#region Models
+
+public abstract class IndicidualAccount
+{
+}
+
+public class DanskeBankIndividualAccount: IndicidualAccount
+{
+
+}
+
+public class CreditBankIndividualAccount : IndicidualAccount
+{
+
+}
+
+#endregion
+
+
+#region Abstrations
+
+// Abstraction
+public abstract class BaseAccountService(BaseAccountCreator implementor)
+{
+    private readonly BaseAccountCreator _implementor = implementor;
+
+    public virtual IndicidualAccount CreateAccount()
+    {
+        return _implementor.CreateAccount();
+    }
+}
+
+// Implementor
+public abstract class BaseAccountCreator
+{
+    public abstract IndicidualAccount CreateAccount();
+}
+
+#endregion
+
+#region Implementation
+
+// Refined abstraction
+public class AccountService : BaseAccountService
+{
+    public AccountService(BaseAccountCreator implementor) : base(implementor)
+    {
+    }
+}
+
+
+// Concrete implementor 1
+public class DanskeBankAccountCreator : BaseAccountCreator
+{
+    public override IndicidualAccount CreateAccount()
+    {
+        return new DanskeBankIndividualAccount();
+    }
+}
+
+// Concrete implementor2
+public class CreditBankAccountCreator : BaseAccountCreator
+{
+    public override IndicidualAccount CreateAccount()
+    {
+        return new CreditBankIndividualAccount();
+    }
+}
+
+#endregion
---
+++ DesignPattern.Application/Structural/Decorator.cs
@@ -0,0 +1,69 @@
+﻿namespace DesignPattern.Application.Structural.Decorator;
+
+// Attaches additional responsibilities to an object dynamically
+
+#region Usage
+
+static class Program
+{
+    static void Main()
+    {
+        var account = new DanskeBankIndividualAccount();
+        var orderableAccount = new DanskeBankOrderableAccount(account);
+        orderableAccount.Buy();
+    }
+}
+
+#endregion
+
+#region Models
+
+// Component
+public abstract class IndicidualAccount
+{
+}
+
+// Concrete component
+public class DanskeBankIndividualAccount : IndicidualAccount
+{
+
+}
+
+// Abstract decorator
+public abstract class OrderableAccount : IndicidualAccount
+{
+
+    protected IndicidualAccount _account;
+
+    protected OrderableAccount(IndicidualAccount account)
+    {
+        _account = account;
+    }
+
+    public abstract void Buy();
+}
+
+#endregion
+
+#region Implementation
+
+// Concrete decorator
+public class DanskeBankOrderableAccount : OrderableAccount
+{
+    public DanskeBankOrderableAccount(IndicidualAccount account) : base(account)
+    {
+    }
+
+    public override void Buy()
+    {
+        // TODO: business logic here...
+        Log();
+    }
+
+    public void Log()
+    {
+        Console.WriteLine("Someone is buying Danske Bank account");
+    }
+}
+
+#endregion
